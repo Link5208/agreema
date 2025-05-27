c2be03dba231db3c28f157fe80f6f2322e009760
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-05-27 16:35:51 +0700

    complete config

--- src/main/java/com/example/demo/AgreemaApplication.java
+++ src/main/java/com/example/demo/AgreemaApplication.java
@@ -3,12 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
-//disable security
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
-})
+@SpringBootApplication
+// disable security
+// @SpringBootApplication(exclude = {
+// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+// org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+// })
 
 public class AgreemaApplication {
 
