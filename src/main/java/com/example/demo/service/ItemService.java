98b4305306f3ab26b36d370a0e9cf171fb1e9802
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-05-31 17:04:45 +0700

    change item controller

--- src/main/java/com/example/demo/service/ItemService.java
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -5,13 +5,20 @@ import org.springframework.data.jpa.domain.Specification;
 
 import com.example.demo.domain.Item;
 import com.example.demo.domain.response.ResultPaginationDTO;
+import com.example.demo.util.error.IdInvalidException;
 
 public interface ItemService {
 	public Item handleSaveItem(Item item);
 
-	public Item handleFetchById(long id);
+	public Item fetchById(long id);
 
 	public ResultPaginationDTO handleFetchAllItems(Specification<Item> specification, Pageable pageable);
 
-	public void handleDelete(long id);
+	public void handleDelete(long id) throws IdInvalidException;
+
+	public Item handleCreateItem(Item postmanItem) throws IdInvalidException;
+
+	public Item handleUpdateItem(Item postmanItem) throws IdInvalidException;
+
+	public Item handleFetchItemById(long id) throws IdInvalidException;
 }
--- src/main/java/com/example/demo/service/impl/ItemServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ItemServiceImpl.java
@@ -7,10 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.Contract;
 import com.example.demo.domain.Item;
 import com.example.demo.domain.response.ResultPaginationDTO;
 import com.example.demo.repository.ItemRepository;
+import com.example.demo.service.ContractService;
 import com.example.demo.service.ItemService;
+import com.example.demo.util.error.IdInvalidException;
 
 import lombok.AllArgsConstructor;
 
@@ -18,12 +21,13 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ItemServiceImpl implements ItemService {
 	private final ItemRepository itemRepository;
+	private final ContractService contractService;
 
 	public Item handleSaveItem(Item item) {
 		return this.itemRepository.save(item);
 	}
 
-	public Item handleFetchById(long id) {
+	public Item fetchById(long id) {
 		Optional<Item> optional = this.itemRepository.findById(id);
 		return optional.isPresent() ? optional.get() : null;
 	}
@@ -45,7 +49,52 @@ public class ItemServiceImpl implements ItemService {
 		return result;
 	}
 
-	public void handleDelete(long id) {
-		this.itemRepository.deleteById(id);
+	// public void handleDelete(long id) {
+	// this.itemRepository.deleteById(id);
+	// }
+	public void handleDelete(long id) throws IdInvalidException {
+		Item currItem = fetchById(id);
+		if (currItem == null) {
+			throw new IdInvalidException("Item ID = " + id + " doesn't exist!");
+		}
+		currItem.setDeleted(true);
+		handleSaveItem(currItem);
+	}
+
+	public Item handleCreateItem(Item postmanItem) throws IdInvalidException {
+		Item currItem = fetchById(postmanItem.getId());
+		if (currItem != null) {
+			throw new IdInvalidException("Item ID = " + postmanItem.getId() + " already exists");
+		}
+
+		postmanItem.setTotal(postmanItem.getQuantity() * postmanItem.getPrice());
+		postmanItem.setDeleted(false);
+		return handleSaveItem(postmanItem);
+	}
+
+	public Item handleUpdateItem(Item postmanItem) throws IdInvalidException {
+		Item currItem = fetchById(postmanItem.getId());
+		if (currItem == null) {
+			throw new IdInvalidException("Item ID = " + postmanItem.getId() + " doesn't exist!");
+		}
+		currItem.setName(postmanItem.getName());
+		currItem.setUnit(postmanItem.getUnit());
+		currItem.setQuantity(postmanItem.getQuantity());
+		currItem.setPrice(postmanItem.getPrice());
+		postmanItem.setTotal(postmanItem.getQuantity() * postmanItem.getPrice());
+
+		if (postmanItem.getContract() != null) {
+			Contract currContract = this.contractService.findContractById(postmanItem.getContract().getId());
+			currItem.setContract(currContract);
+		}
+		return handleSaveItem(currItem);
+	}
+
+	public Item handleFetchItemById(long id) throws IdInvalidException {
+		Item currItem = fetchById(id);
+		if (currItem == null) {
+			throw new IdInvalidException("Item ID = " + id + " doesn't exist!");
+		}
+		return currItem;
 	}
 }
